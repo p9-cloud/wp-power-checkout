29a2d6e1c6487db9f62bae4c868d218661014f5e
J7 <j7.dev.gg@gmail.com>
2025-10-30 17:11:40 +0800

    feat: 檢查 Powerhouse 版本兼容

--- inc/classes/Bootstrap.php
+++ inc/classes/Bootstrap.php
@@ -4,9 +4,7 @@ declare ( strict_types = 1 );
 
 namespace J7\PowerCheckout;
 
-use J7\PowerCheckout\Utils\Base;
-use J7\WpUtils\Classes\WP;
-use Kucrut\Vite;
+use J7\PowerCheckout\Plugin;
 
 if ( class_exists( 'J7\PowerCheckout\Bootstrap' ) ) {
 	return;
@@ -18,6 +16,10 @@ final class Bootstrap {
 
 	/** Constructor */
 	public function __construct() {
+		$is_compatible = self::check_compatibility();
+		if (! $is_compatible ) {
+			return;
+		}
 
 		Domains\Payment\Loader::register_hooks();
 		Domains\Settings\Services\SettingApiService::register_hooks();
@@ -25,6 +27,42 @@ final class Bootstrap {
 
 		\add_action( 'before_woocommerce_init', [ __CLASS__, 'declare_compatibility' ] );
 		\add_action('admin_head', [ __CLASS__, 'custom_css' ]);
+	}
+
+	/**
+	 * 檢查與 Powerhouse 外掛的相容性
+	 *
+	 * @return bool 回傳 Powerhouse 版本與相容性結果
+	 */
+	private static function check_compatibility(): bool {
+		// 如果沒有安裝 Powerhouse ，就不用考慮相容性
+		if (!\class_exists( '\J7\Powerhouse\Plugin')) {
+			return true;
+		}
+
+		$min_ver       = '3.3.38';
+		$ver           = \J7\Powerhouse\Plugin::$version;
+		$is_compatible = \version_compare($ver, $min_ver, '>=');
+
+		if (!$is_compatible) {
+			\add_action(
+			'admin_notices',
+			static function () use ( $ver, $min_ver ) {
+				\printf(
+					'<div class="notice notice-error is-dismissible">
+                                    <p><strong>錯誤：</strong>%1$s 無法啟用，Powerhouse 當前版本為 v%2$s 最小版本要求為 v%3$s，<a href="%4$s" target="_blank">前往安裝</a></p>
+                                </div>
+                                ',
+					Plugin::$app_name,
+					$ver,
+					$min_ver,
+					\admin_url('plugins.php')
+				);
+			}
+			);
+		}
+
+		return $is_compatible;
 	}
 
 
